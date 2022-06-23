546ef8badf415af8415d28e5b7a09c4d1912f717
sadiya <104234800+sadiya-szn@users.noreply.github.com>
2022-06-22 18:36:21 -0700

    new commit

---
+++ index.js
@@ -0,0 +1,3 @@
+document.getElementById("name").innerHTML = "Hello my name is Sadiya";
+document.getElementById("height").innerHTML = "I am 170cm tall";
+document.getElementById("country").innerHTML = "I am from Nigeria";
